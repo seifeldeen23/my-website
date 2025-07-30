0db3d38553c4a082055c68224da63785324406d5
seifeldeen23 <seifeldeenayman2@gmail.com>
2025-07-30 23:48:25 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// ممكن نضيف حركات بعدين، دلوقتي فاضي
+// زر العودة للأعلى
+const backToTop = document.getElementById("backToTop");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+});
+backToTop.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
